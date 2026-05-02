2ecf40f06331a7ba19b8069b1c2b204707d41aab
someshramisetty <someshramisetty@gmail.com>
2026-05-02 13:03:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("gold22").innerText = "22K Gold: ₹6,500/g";
+document.getElementById("gold24").innerText = "24K Gold: ₹7,100/g";
+document.getElementById("updated").innerText = "Updated: Today";
